228a0ad3fb387cc4647ccaa1a3c5a19a37021e37
Kenneth-Mah <kennethmah@rocketmail.com>
2024-01-06 11:42:33 +0800

    Connect React App to Get All Employees REST API

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,27 +1,17 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { listEmployees } from '../services/EmployeeService'
 
 const ListEmployeeComponent = () => {
 
-    const dummyData = [
-        {
-            "id": 1,
-            "firstName": "Ramesh",
-            "lastName": "Fadatare",
-            "email": "ramesh@gmail.com"
-        },
-        {
-            "id": 2,
-            "firstName": "Umesh",
-            "lastName": "Fadatare",
-            "email": "umesh@gmail.com"
-        },
-        {
-            "id": 3,
-            "firstName": "Rajkumar",
-            "lastName": "Fadatare",
-            "email": "rajkumar@gmail.com"
-        }
-    ]
+    const [employees, setEmployees] = useState([])
+
+    useEffect(() => {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }, [])
 
   return (
     <div className='container'>
@@ -37,7 +27,7 @@ const ListEmployeeComponent = () => {
             </thead>
             <tbody>
                 {
-                    dummyData.map(employee =>
+                    employees.map(employee =>
                         <tr key={employee.id}>
 
                             <td>{employee.id}</td>
